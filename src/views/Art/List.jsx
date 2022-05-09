f01dd885e96a1e3c8f722a23e75fc1fe26c221e4
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-09 14:56:23 -0700

    List.jsx fetching from api and mapping over art

--- src/views/Art/List.jsx
+++ src/views/Art/List.jsx
@@ -1,9 +1,35 @@
 import { useEffect, useState } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
 import ArtCard from '../../components/Art/Card';
 
 export default function ArtList() {
+  const [artWorks, setArtWorks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchArt() {
+      const res = await fetch(
+        'https://api.artic.edu/api/v1/artworks/search?fields=id,title,image_id,artist_title,&limit=12&page=1&q=poppy'
+      );
+
+      const results = await res.json();
+      setArtWorks(results.data);
+      setLoading(false);
+    }
+    fetchArt();
+  }, []);
+
   return (
-    <div>ArtList</div>
-  )
+    <>
+      <h3>Poppy Art</h3>
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <div>
+          {artWorks.map((art) => {
+            return <ArtCard key={art.id} art={art} />;
+          })}
+        </div>
+      )}
+    </>
+  );
 }

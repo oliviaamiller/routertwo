314f1fb31fb872dab53d155ccabf2404e3e439a0
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-09 14:48:52 -0700

    fixed bug in App.jsx, routes now being returned

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import ArtList from './views/Art/List';
 import ArtDetails from './views/Art/Details';
 
 export default function App() {
-  return;
-  <>
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/art" />
-      </Route>
-      <Route path="/art">
-        <ArtList />
-      </Route>
-      <Route path="/art/:id">
-        <Link to="/">{'Back to All Art'}</Link>
-        <ArtDetails />
-      </Route>
-    </Switch>
-  </>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/art" />
+        </Route>
+        <Route path="/art">
+          <ArtList />
+        </Route>
+        <Route path="/art/:id">
+          <Link to="/">{'Back to All Art'}</Link>
+          <ArtDetails />
+        </Route>
+      </Switch>
+    </>
+  );
 }
--- src/views/Art/List.jsx
+++ src/views/Art/List.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import ArtCard from '../../components/Art/Card';
 
 export default function ArtList() {
   return (

0afcd91eb7ee322fc325e1c74af158dae24530c1
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-09 14:43:24 -0700

    added switch and routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
+import { Route, Switch, Redirect, Link } from 'react-router-dom';
+import ArtList from './views/Art/List';
+import ArtDetails from './views/Art/Details';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return;
+  <>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/art" />
+      </Route>
+      <Route path="/art">
+        <ArtList />
+      </Route>
+      <Route path="/art/:id">
+        <Link to="/">{'Back to All Art'}</Link>
+        <ArtDetails />
+      </Route>
+    </Switch>
+  </>;
 }
---
+++ src/views/Art/Details.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArtDetails() {
+  return (
+    <div>ArtDetails</div>
+  )
+}

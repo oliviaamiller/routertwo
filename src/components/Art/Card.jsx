f486983362c7074f075a2a5ad0d6650b7b6bf8f3
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-05-10 17:58:03 -0700

    added css for cards

--- src/components/Art/Card.jsx
+++ src/components/Art/Card.jsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
 import styles from '../../App.css';
 
-
 export default function ArtCard({ art }) {
   return (
     <>
-    <div className={styles.card}>
-      <Link to={`/art/${art.id}`}>
-        <img
-          className={styles.img}
-          src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
-          alt={`${art.artwork_type_title} of eyes by ${art.artist_title}`} />
-          <p>{art.title}</p>
-          <p>{art.artist_title}</p>
-      </Link>
-    </div>
+      <div>
+        <Link to={`/art/${art.id}`} className={styles.card}>
+          <img
+            className={styles.img}
+            src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+            alt={`${art.artwork_type_title} of eyes by ${art.artist_title}`}
+          />
+          <div className={styles.cardData}>
+            <p className={styles.title}>{art.title}</p>
+            <p className={styles.artist}>{art.artist_title}</p>
+          </div>
+        </Link>
+      </div>
     </>
-  )
+  );
 }
